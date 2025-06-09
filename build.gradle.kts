7a35a668267c9a06f7655f22e79c57c9c85698cf
twoweekhee <twoweekhee@ijuhuiui-Macmini.local>
2025-06-09 23:44:30 +0900

    🔧 Add redis config
    add redisTemplate, stringRedisTemplate, lettuceConeectionFactory

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,12 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/twoweekhee/coupon/config/RedisConfig.kt
+++ src/main/kotlin/com/twoweekhee/coupon/config/RedisConfig.kt
@@ -1,4 +1,50 @@
 package com.twoweekhee.coupon.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
 class RedisConfig {
+
+    @Bean
+    fun RedisTemplate(): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>().apply {
+            keySerializer = StringRedisSerializer()
+            hashKeySerializer = StringRedisSerializer()
+
+            valueSerializer = GenericJackson2JsonRedisSerializer()
+            hashValueSerializer = GenericJackson2JsonRedisSerializer()
+
+            setDefaultSerializer(GenericJackson2JsonRedisSerializer())
+            connectionFactory = LettuceConnectionFactory()
+
+            afterPropertiesSet()
+        }
+
+        return redisTemplate
+    }
+
+    @Bean
+    fun StringRedisTemplate(): StringRedisTemplate {
+
+        return StringRedisTemplate().apply {
+            connectionFactory = LettuceConnectionFactory()
+        }
+    }
+
+    @Bean
+    fun lettuceConnectionFactory(): LettuceConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration().apply {
+            hostName = "localhost"
+            port = 6379
+        }
+
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
+    }
 }
