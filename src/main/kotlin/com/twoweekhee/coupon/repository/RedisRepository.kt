f40bf640f4ef3aa13fb4a198b3c84dea7db997dd
twoweekhee@icloud.com <myjjoo4758@mealgen.net>
2025-06-11 13:55:47 +0900

    ✨ Redis repository
    save coupons

--- src/main/kotlin/com/twoweekhee/coupon/repository/RedisRepository.kt
+++ src/main/kotlin/com/twoweekhee/coupon/repository/RedisRepository.kt
@@ -1,4 +1,22 @@
 package com.twoweekhee.coupon.repository
 
-class RedisRepository {
+import com.twoweekhee.coupon.domain.Coupon
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Repository
+
+@Repository
+class RedisRepository(
+  private val stringRedisTemplate: RedisTemplate<String, String>
+) {
+  companion object {
+    const val COUPON_POOL = "coupon:pool"
+    const val COUPON_DETAILS = "coupon:details"
+    const val USER_COUPON_MAP = "user:coupon"
+    const val ISSUED_COUPONS = "coupon:issued"
+    const val COUPON_COUNTER = "coupon:counter"
+  }
+
+  fun saveCouponsIdList(counponIds: List<String>) {
+    stringRedisTemplate.opsForList().rightPushAll(COUPON_POOL, counponIds)
+  }
 }
--- src/main/kotlin/com/twoweekhee/coupon/service/CouponService.kt
+++ src/main/kotlin/com/twoweekhee/coupon/service/CouponService.kt
@@ -2,14 +2,18 @@ package com.twoweekhee.coupon.service
 
 import com.twoweekhee.coupon.domain.Coupon
 import com.twoweekhee.coupon.logger
+import com.twoweekhee.coupon.repository.RedisRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class CouponService {
+class CouponService(
+    private val redisRepository: RedisRepository
+) {
 
     fun generateCoupons(poolNumber: Int): List<Coupon> {
         val coupons = mutableListOf<Coupon>()
+        val couponIds = mutableListOf<String>()
         val generatedCouponsCode = mutableSetOf<String>()
 
         logger.info("쿠폰 생성 시작: $poolNumber 개")
@@ -27,9 +31,12 @@ class CouponService {
                 id = "COUPON_${index + 1}개",
                 code = couponCode
             )
+            couponIds.add(coupon.id)
             coupons.add(coupon)
         }
         logger.info("쿠폰 생성 완료: ${coupons.size} 개")
+
+        redisRepository.saveCouponsIdList(couponIds)
         return coupons
     }
 
